28c448d649435d39430b87d29ba20f693d980af1
emilys314 <emilys314@protonmail.com>
2023-06-08 13:36:52 -0400

    package setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+      <article className="prose lg:prose-xl">
+        <h1>Garlic bread with cheese: What the science tells us</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          For years parents have espoused the health benefits of eating garlic bread with cheese to
+          their children, with the food earning such an iconic status in our culture that kids will
+          often dress up as warm, cheesy loaf for Halloween.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked to a series of rabies
+          cases springing up around the country.
+        </p>
+      </article>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
