3ff40354d77f811aeabeeaa8c35e609e2de7ee5c
emilys314 <emilys314@protonmail.com>
2023-06-10 11:25:29 -0400

    navbar mobile, mostly functional

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -13,83 +13,19 @@ export const NavBar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    // <div className="navbar bg-base-100">
-    //   <div className="navbar-start">
-    //     {/* Mobile */}
-    //     <div className="dropdown">
-    //       <label tabIndex={0} className="btn btn-ghost lg:hidden">
-    //         <svg
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           className="h-5 w-5"
-    //           fill="none"
-    //           viewBox="0 0 24 24"
-    //           stroke="currentColor"
-    //         >
-    //           <path
-    //             strokeLinecap="round"
-    //             strokeLinejoin="round"
-    //             strokeWidth="2"
-    //             d="M4 6h16M4 12h8m-8 6h16"
-    //           />
-    //         </svg>
-    //       </label>
-
-    //       <ul
-    //         tabIndex={0}
-    //         className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-    //       >
-    //         <li>
-    //           <a>Item 1</a>
-    //         </li>
-    //         <li>
-    //           <a>Item 3</a>
-    //         </li>
-    //       </ul>
-    //     </div>
-
-    //     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-    //   </div>
-
-    //   {/* Desktop */}
-    //   <div className="navbar-center hidden lg:flex">
-    //     <ul className="menu menu-horizontal px-1">
-    //       <li>
-    //         <a>Item 1</a>
-    //       </li>
-    //       <li>
-    //         <a>Item 3</a>
-    //       </li>
-    //     </ul>
-    //   </div>
-
-    //   <div className="navbar-end">
-    //     <a className="btn">Button</a>
-    //   </div>
-    // </div>
-
-    <header className="">
+    <header>
       {/* desktop */}
-      <nav className="navbar bg-base-100 py-0" aria-label="Global">
+      <nav
+        className="navbar bg-base-100 py-0 sm:ring-1 sm:ring-neutral"
+        aria-label="Global"
+      >
         <div className="navbar-start">
           <label
             tabIndex={0}
             className="btn btn-ghost md:hidden"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+            <Bars3Icon className="h-8 w-8" />
           </label>
 
           <a href="#" className="btn btn-ghost normal-case">
@@ -120,60 +56,44 @@ export const NavBar = () => {
 
       <Dialog
         as="div"
-        className="lg:hidden"
+        className="md:hidden"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center gap-x-6">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
-            <a
-              href="#"
-              className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign up
-            </a>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-base-100 p-2 sm:max-w-sm sm:ring-1 sm:ring-neutral">
+          <ul className="menu menu-horizontal flex items-center gap-x-6">
+            <li>
+              <a href="#" className="">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+            </li>
+            <li className="ml-auto">
+              <button
+                type="button"
+                className="btn"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-8 w-8" aria-hidden="true" />
+              </button>
+            </li>
+          </ul>
 
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
+          <ul className="menu menu-lg rounded-box">
+            <li>
+              <a>Classes</a>
+            </li>
+            <li>
+              <a>Get Involved</a>
+            </li>
+            <li></li>
+            <a className="btn btn-block mt-4">Donate</a>
+          </ul>
         </Dialog.Panel>
       </Dialog>
     </header>
