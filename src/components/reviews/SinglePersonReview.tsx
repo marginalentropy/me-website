6ebfc391be65fb2ae004c42ae9c5c993c84d0fbe
emilys314 <emilys314@protonmail.com>
2023-06-13 20:45:05 -0400

    update review / social proof

---
+++ src/components/reviews/SinglePersonReview.tsx
@@ -0,0 +1,33 @@
+const review = {
+  content: `
+    <p>An excellent class and phenomenal teachers, thanks for all you do to help educate and empower the next generation(s)!</p>
+  `,
+  author: "Zachary Bohnstedt (Parent):",
+  avatarSrc:
+    "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+};
+
+export const SinglePersonReview = () => {
+  return (
+    <div className="p-8 flex items-center bg-base-200 ">
+      {/* <a href="https://www.linkedin.com" className="mx-auto"> */}
+      <img
+        src={review.avatarSrc}
+        alt={`${review.author}.`}
+        className="h-14 w-14 rounded-full"
+      />
+      {/* </a> */}
+      <div className="flex-col px-4">
+        <div className="">
+          <h4 className="text-sm font-bold text-base-content">
+            {review.author}
+          </h4>
+        </div>
+        <div
+          className="text-base italic text-base-content "
+          dangerouslySetInnerHTML={{ __html: review.content }}
+        />
+      </div>
+    </div>
+  );
+};
--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { Layout } from "../../components/layouts/Layout";
+import { SinglePersonReview } from "../../components/reviews/SinglePersonReview";
 import { Hero } from "./Hero";
 
 export const LandingPage = () => {
@@ -6,51 +7,10 @@ export const LandingPage = () => {
     <Layout>
       <Hero />
 
-      <div className="bg-base-200">
-        <div className="flex w-full p-4 max-w-7xl mx-auto">
-          <div className="grid h-20 flex-grow card place-items-center">
-            Top Supporters:
-          </div>
-          <div className="divider divider-horizontal"></div>
-          <div className="grid h-20 flex-grow-[2] place-items-center">
-            Excella
-          </div>
-
-          {/* <div className="grid h-40 flex-grow card place-items-center">
-            Great review blahblahblah
-          </div>
-          <div className="grid h-40 flex-grow-[2] place-items-center">
-            Great review blahblahblah
-          </div> */}
-        </div>
-      </div>
+      <SinglePersonReview />
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 bg-base-100 p-4 pt-16 max-w-7xl mx-auto">
-        <div className="card bg-base-300 max-w-md md:max-w-xs mx-auto">
-          <div className="card-body items-center text-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.712 4.33a9.027 9.027 0 011.652 1.306c.51.51.944 1.064 1.306 1.652M16.712 4.33l-3.448 4.138m3.448-4.138a9.014 9.014 0 00-9.424 0M19.67 7.288l-4.138 3.448m4.138-3.448a9.014 9.014 0 010 9.424m-4.138-5.976a3.736 3.736 0 00-.88-1.388 3.737 3.737 0 00-1.388-.88m2.268 2.268a3.765 3.765 0 010 2.528m-2.268-4.796a3.765 3.765 0 00-2.528 0m4.796 4.796c-.181.506-.475.982-.88 1.388a3.736 3.736 0 01-1.388.88m2.268-2.268l4.138 3.448m0 0a9.027 9.027 0 01-1.306 1.652c-.51.51-1.064.944-1.652 1.306m0 0l-3.448-4.138m3.448 4.138a9.014 9.014 0 01-9.424 0m5.976-4.138a3.765 3.765 0 01-2.528 0m0 0a3.736 3.736 0 01-1.388-.88 3.737 3.737 0 01-.88-1.388m2.268 2.268L7.288 19.67m0 0a9.024 9.024 0 01-1.652-1.306 9.027 9.027 0 01-1.306-1.652m0 0l4.138-3.448M4.33 16.712a9.014 9.014 0 010-9.424m4.138 5.976a3.765 3.765 0 010-2.528m0 0c.181-.506.475-.982.88-1.388a3.736 3.736 0 011.388-.88m-2.268 2.268L4.33 7.288m6.406 1.18L7.288 4.33m0 0a9.024 9.024 0 00-1.652 1.306A9.025 9.025 0 004.33 7.288"
-              />
-            </svg>
-            <h2 className="card-title">Accessibility</h2>
-            <p>
-              We provide completely cost-free coding class, based on Microsoft's
-              Minecraft Education curriculum. We also provide laptops and
-              hardware for students who don't have access.
-            </p>
-          </div>
-        </div>
-        <div className="card  bg-base-300 max-w-md md:max-w-xs mx-auto">
+        <div className="card max-w-md md:max-w-xs mx-auto">
           <div className="card-body items-center text-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -75,7 +35,7 @@ export const LandingPage = () => {
             </p>
           </div>
         </div>
-        <div className="card  bg-base-300 max-w-md md:max-w-xs mx-auto">
+        <div className="card max-w-md md:max-w-xs mx-auto">
           <div className="card-body items-center text-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -97,6 +57,30 @@ export const LandingPage = () => {
               of understanding, not malice. We address this by creating an
               empathetic and communicative environment to equip students with
               tools to treat others and themselves with kindness and empathy.
+            </p>
+          </div>
+        </div>
+        <div className="card max-w-md md:max-w-xs mx-auto">
+          <div className="card-body items-center text-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.712 4.33a9.027 9.027 0 011.652 1.306c.51.51.944 1.064 1.306 1.652M16.712 4.33l-3.448 4.138m3.448-4.138a9.014 9.014 0 00-9.424 0M19.67 7.288l-4.138 3.448m4.138-3.448a9.014 9.014 0 010 9.424m-4.138-5.976a3.736 3.736 0 00-.88-1.388 3.737 3.737 0 00-1.388-.88m2.268 2.268a3.765 3.765 0 010 2.528m-2.268-4.796a3.765 3.765 0 00-2.528 0m4.796 4.796c-.181.506-.475.982-.88 1.388a3.736 3.736 0 01-1.388.88m2.268-2.268l4.138 3.448m0 0a9.027 9.027 0 01-1.306 1.652c-.51.51-1.064.944-1.652 1.306m0 0l-3.448-4.138m3.448 4.138a9.014 9.014 0 01-9.424 0m5.976-4.138a3.765 3.765 0 01-2.528 0m0 0a3.736 3.736 0 01-1.388-.88 3.737 3.737 0 01-.88-1.388m2.268 2.268L7.288 19.67m0 0a9.024 9.024 0 01-1.652-1.306 9.027 9.027 0 01-1.306-1.652m0 0l4.138-3.448M4.33 16.712a9.014 9.014 0 010-9.424m4.138 5.976a3.765 3.765 0 010-2.528m0 0c.181-.506.475-.982.88-1.388a3.736 3.736 0 011.388-.88m-2.268 2.268L4.33 7.288m6.406 1.18L7.288 4.33m0 0a9.024 9.024 0 00-1.652 1.306A9.025 9.025 0 004.33 7.288"
+              />
+            </svg>
+            <h2 className="card-title">Accessibility</h2>
+            <p>
+              We provide completely cost-free coding class, based on Microsoft's
+              Minecraft Education curriculum. We also provide laptops and
+              hardware for students who don't have access.
             </p>
           </div>
         </div>
