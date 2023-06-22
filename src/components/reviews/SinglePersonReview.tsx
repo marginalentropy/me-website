0acd9b657f64ac56f8de272d882d5bff9fc96a22
emilys314 <emilys314@protonmail.com>
2023-06-22 16:52:08 -0400

    use correct link / image for review

--- src/components/reviews/SinglePersonReview.tsx
+++ src/components/reviews/SinglePersonReview.tsx
@@ -1,23 +1,24 @@
+import zachLinkedInPic from "../../assets/zach-linkedin-pic.jpg";
+
 const review = {
-  content: `
-    <p>An excellent class and phenomenal teachers, thanks for all you do to help educate and empower the next generation(s)!</p>
-  `,
-  author: "Zachary Bohnstedt (Parent):",
-  avatarSrc:
-    "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+  content: `An excellent class and phenomenal teachers, thanks for all you do to help educate and empower the next generation(s)!`,
+  author: "Zachary Bohnstedt (Parent)",
+  avatarSrc: zachLinkedInPic,
+  avatarUrl:
+    "https://www.linkedin.com/feed/update/urn:li:activity:7072917277733519360?commentUrn=urn%3Ali%3Acomment%3A%28activity%3A7072917277733519360%2C7072952406308761601%29&dashCommentUrn=urn%3Ali%3Afsd_comment%3A%287072952406308761601%2Curn%3Ali%3Aactivity%3A7072917277733519360%29",
 };
 
 export const SinglePersonReview = () => {
   return (
     <div className=" flex bg-base-200 p-8">
       <div className="mx-auto flex max-w-7xl items-center">
-        {/* <a href="https://www.linkedin.com" className="mx-auto"> */}
-        <img
-          src={review.avatarSrc}
-          alt={`${review.author}.`}
-          className="h-14 w-14 rounded-full"
-        />
-        {/* </a> */}
+        <div className="avatar">
+          <div className="w-16 rounded-full">
+            <a href={review.avatarUrl}>
+              <img src={review.avatarSrc} alt={`${review.author}.`} />
+            </a>
+          </div>
+        </div>
         <div className="flex-col px-4">
           <div className="">
             <h4 className="text-sm font-bold text-base-content">
