6810d8739b07e83a143759c702753cd593739aaa
emilys314 <emilys314@protonmail.com>
2023-07-08 19:16:03 -0400

    Convert SupportUs to prose

--- src/pages/landingPage/SupportUs.tsx
+++ src/pages/landingPage/SupportUs.tsx
@@ -20,23 +20,23 @@ const type = [
 export const SupportUs = () => {
   return (
     <div className="pb-32 pt-16">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-4xl sm:text-center">
-          <p className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">
+      <div className="mx-auto prose max-w-7xl px-6 lg:px-8">
+        <div className=" mx-auto max-w-4xl sm:text-center">
+          <h1>
             Support Us
-          </p>
+          </h1>
         </div>
-        <div className="mt-20 flow-root">
+        <div className="mt-10 flow-root">
           <div className="isolate -mt-16 grid max-w-sm grid-cols-1 gap-y-16 divide-y divide-base-300  sm:mx-auto lg:-mx-8 lg:mt-0 lg:max-w-none lg:grid-cols-3 lg:divide-x lg:divide-y-0 xl:-mx-4">
             {type.map((tier, index) => (
-              <div key={index} className="pt-16 lg:px-8 lg:pt-0 xl:px-14 ">
-                <h3
+              <div key={index} className="pt-8 lg:px-8 lg:pt-0 xl:px-14 ">
+                <h2
                   id={index.toString()}
                   className="text-base font-semibold leading-7"
                 >
                   {tier.name}
-                </h3>
-                <p className="mt-3 text-sm leading-6 text-gray-500">
+                </h2>
+                <p className="text-sm leading-6 text-gray-500">
                   {tier.description}
                 </p>
                 <a
